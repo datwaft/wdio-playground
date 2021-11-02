ce46e63979b8c36c1719c6e032c24a25bc066af1
datwaft <datwaftx@gmail.com>
2021-11-02 11:48:46 -0600

    format: prettier

--- test/pageobjects/secure.page.js
+++ test/pageobjects/secure.page.js
@@ -1,13 +1,9 @@
-const Page = require('./page');
+const Page = require("./page");
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
 class SecurePage extends Page {
-    /**
-     * define selectors using getter methods
-     */
-    get flashAlert () { return $('#flash') }
+  get flashAlert() {
+    return $("#flash");
+  }
 }
 
 module.exports = new SecurePage();
